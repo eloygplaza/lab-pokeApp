8467763a18ef4559fd62a865fdbb704b9c2b8b19
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-02 21:11:16 +0200

    setup done

---
+++ js/jscript.js
@@ -0,0 +1,7 @@
+function fetchAllPokemon() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then((response) => response.json())
+    .then((allpokemon) => console.log(allpokemon));
+}
+
+fetchAllPokemon();
