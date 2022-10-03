4b0a8c3b7d2c63d71b13e867792bbdc6f1c26d90
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-03 09:37:14 +0200

    all done

--- js/jscript.js
+++ js/jscript.js
@@ -1,7 +1,34 @@
-function fetchAllPokemon() {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-    .then((response) => response.json())
-    .then((allpokemon) => console.log(allpokemon));
+async function fetchAllPokemon() {
+  let allPokemon;
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+
+  allPokemon = await res.json();
+
+  let container = document.querySelector(".is-danger");
+
+  // iteramos por todos los pokemons
+  allPokemon["results"].forEach((element) => {
+    // id del pokemon
+    let pokemonId = element.url.split("/")[6];
+    // url de la imagen
+    let imagePath = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+
+    // creamos lo elementos a insertar en el html
+    const block = document.createElement("a");
+    block.classList.add("panel-block");
+    const blockSpan = document.createElement("span");
+    const spanImg = document.createElement("img");
+    spanImg.src = imagePath;
+    spanImg.alt = element.name;
+    const spanText = document.createTextNode(`${element.name}`);
+
+    // anexamos e insertamos en html
+    blockSpan.appendChild(spanImg);
+    block.appendChild(blockSpan);
+    block.appendChild(spanText);
+    container.appendChild(block);
+  });
 }
 
+// ejecutamos la funcion
 fetchAllPokemon();
